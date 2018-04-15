f9115e51a62f94d5d9f4c4b06b06b0902652680b
Alysson Gomes <alyssonggs@hotmail.com>
2018-04-14 22:29:57 -0300

    Criação da estrutura de pilha

---
+++ lib/george_compiler/stack.ex
@@ -0,0 +1,21 @@
+defmodule Stack do
+    @moduledoc """
+        Módulo genério de pilha utilizado no SMC.
+        Copiado de https://onor.io/2014/06/02/a-naive-stack-implementation-in-elixir-2/
+    """ 
+    defstruct elements: []
+     
+    def new, do: %Stack{}
+     
+    def push(stack, element) do
+        %Stack{stack | elements: [element | stack.elements]}
+    end
+     
+    def pop(%Stack{elements: []}), do: raise("Stack is empty!")
+
+    def pop(%Stack{elements: [top | rest]}) do
+        {top, %Stack{elements: rest}}
+    end
+     
+    def depth(%Stack{elements: elements}), do: length(elements)
+end
