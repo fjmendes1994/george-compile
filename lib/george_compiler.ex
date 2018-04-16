578c9c587c68ae1a42a27b045c6e5e59fd554fbd
Fernando José da Silva Mendes <fjmendes1994@hotmail.com>
2018-04-16 18:18:17 -0300

    Operações aritimeticas, booleanas e comandos de acordo com a gramatica da IMP

--- lib/george_compiler.ex
+++ lib/george_compiler.ex
@@ -3,8 +3,4 @@ defmodule GeorgeCompiler do
    Compilador Geroge
   """
 
-  GeorgeCompiler.Parser.parse!("2 + 2 - -2 + 2" ) |> IO.inspect
-  GeorgeCompiler.Parser.parse!("2 > 3" ) |> IO.inspect
-  GeorgeCompiler.Parser.parse!("ab := 3" ) |> IO.inspect
-
 end
--- test/george_compiler/parser_test.exs
+++ test/george_compiler/parser_test.exs
@@ -3,31 +3,29 @@ defmodule ParserTest do
 
   use ExUnit.Case
 
+
   test "operacoes aritimeticas" do
-    assert GeorgeCompiler.Parser.parse!("-1 + 2 - -3 * 4 / -5 % 6") ==
+    assert GeorgeCompiler.Parser.parse!("2 + 2 - -2 + 2 * 3 / 3 % 3" ) == ["2", "+", ["2", "-", ["-2", "+", ["2", "*", ["3", "/", ["3", "%", "3"]]]]]]
   end
 
   test "operacoes booleanas" do
-    assert GeorgeCompiler.Parse.parser!("1 + 2 > 3" )
+    assert GeorgeCompiler.Parser.parse!("2 > 3" )  == ["2", ">", "3"]
+    assert GeorgeCompiler.Parser.parse!("2 >= 3" )  == ["2", ">=", "3"]
+    assert GeorgeCompiler.Parser.parse!("-2 == -3" )  == ["-2", "==", "-3"]
+    assert GeorgeCompiler.Parser.parse!("2 < 3" )  == ["2", "<", "3"]
+    assert GeorgeCompiler.Parser.parse!("2 <= 3" )  == ["2", "<=", "3"]
   end
 
-  @doc """
-    Testes da criação da pilha c
-  """
-  # Este teste está quebrando o parser
-  #test "parser para pilha de controle(C) com vazio" do
-  #  assert GeorgeCompiler.Parser.parse("") |> GeorgeCompiler.eval == %Stack{elements: ["5"]}
-  #end
-
-  test "parser para pilha de controle(C) com 1 valor" do
-    assert GeorgeCompiler.Parser.parse("5") |> GeorgeCompiler.SMC.eval == %Stack{elements: ["5"]}
+  test "atribuicao simples" do
+    assert GeorgeCompiler.Parser.parse!("ab := 2" )
   end
 
-  test "parser para pilha de controle(C) com 2 operandos" do
-    assert GeorgeCompiler.Parser.parse("3+2") |> GeorgeCompiler.SMC.eval == %Stack{elements: ["3","2"]}
+  test "atribuicao expressoes aritimeticas" do
+    assert GeorgeCompiler.Parser.parse!("ab := 2 + 3" )
   end
 
-  test "parser para pilha de controle(C) com 3 operandos" do
-    assert GeorgeCompiler.Parser.parse("5+7*2") |> GeorgeCompiler.SMC.eval == %Stack{elements: ["5","7","2"]}
+  test "atribuicao expressoes booleanas" do
+    assert GeorgeCompiler.Parser.parse!("ab := 2 > 3" )
   end
+
 end
