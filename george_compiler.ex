5fa6869696a3d9d60992e5e67256e8723bb99e74
Phelipe Martins <phelipegm@gmail.com>
2018-04-17 01:25:40 -0300

    Inclusao do modulo BPLC e do smc_test

---
+++ george_compiler.ex
@@ -0,0 +1,18 @@
+defmodule GeorgeCompiler do
+  @moduledoc """
+   Compilador Geroge
+  """
+
+  GeorgeCompiler.Parser.parse("-10 - -20 - 30 / 40 * 50 % 60") |> IO.inspect
+
+  # Aqui ta caindo na recursão infinita descobrir porque ( perguntar pro joao)
+  GeorgeCompiler.Parser.parse("2 < (1 + 10)") |> IO.inspect
+
+  GeorgeCompiler.Parser.parse("2 + (1 + 10)") |> IO.inspect
+
+  GeorgeCompiler.Parser.parse("ab := 2") |> IO.inspect 
+
+  GeorgeCompiler.BPLC.toBPLC(:arithmeticOps)["+"] |> IO.puts
+
+  
+end
