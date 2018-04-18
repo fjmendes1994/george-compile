2dbe66c70686a7ed6d5260d360154bfee82edd11
Fernando José da Silva Mendes <fjmendes1994@hotmail.com>
2018-04-18 19:44:00 -0300

    Retirando os testes de comando

--- lib/george_compiler.ex
+++ lib/george_compiler.ex
@@ -9,9 +9,9 @@ defmodule GeorgeCompiler do
     GeorgeCompiler.Parser.parse!("2 + 2 == 3 + 2" )                          |> IO.inspect()
     GeorgeCompiler.Parser.parse!("2 < 3" )                             |> IO.inspect()
     GeorgeCompiler.Parser.parse!("2 <= 3" )                            |> IO.inspect()
-    GeorgeCompiler.Parser.parse!("ab := 2" )                           |> IO.inspect()
-    GeorgeCompiler.Parser.parse!("ab := 2 + 3" )                       |> IO.inspect()
-    GeorgeCompiler.Parser.parse!("ab := 2 > 3" )                       |> IO.inspect()
+#    GeorgeCompiler.Parser.parse!("ab := 2" )                           |> IO.inspect()
+#    GeorgeCompiler.Parser.parse!("ab := 2 + 3" )                       |> IO.inspect()
+#    GeorgeCompiler.Parser.parse!("ab := 2 > 3" )                       |> IO.inspect()
 
 
 end
