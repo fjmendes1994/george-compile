614fb2444cb66fe0564b97b4411d1b9e9ddd4456
Alysson Gomes <alyssonggs@hotmail.com>
2018-04-21 11:26:17 -0300

    GH-17 Recursão sobre a avaliação da pilha C

--- lib/george_compiler/smc.ex
+++ lib/george_compiler/smc.ex
@@ -1,6 +1,11 @@
 defmodule GeorgeCompiler.SMC do
     
     def evaluate(s,m,c) do
-        {s,m,c}
+        if Stack.depth(c) > 0 do
+            {_,c} = Stack.pop(c)
+            evaluate(s,m,c)
+        else
+            {s,m,c}
+        end
     end
 end
--- test/george_compiler/smc_test.exs
+++ test/george_compiler/smc_test.exs
@@ -3,8 +3,13 @@ defmodule SMCTest do
 
   use ExUnit.Case
 
-  test "Faz nada" do
+  test "Verifica pilhas" do
     GeorgeCompiler.SMC.evaluate(Stack.new, Stack.new, Stack.new) == {Stack.new, Stack.new, Stack.new}
+  end
+
+  test "Esvazia pilha c e para" do
+    c = Stack.new |> Stack.push(5) |> Stack.push(6)
+    GeorgeCompiler.SMC.evaluate(Stack.new, Stack.new, c) == {Stack.new, Stack.new, Stack.new}
   end
 
 end
