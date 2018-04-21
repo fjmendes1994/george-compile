86899f14fa1a2b2515e1a41a86b98b7acf04de00
Alysson Gomes <alyssonggs@hotmail.com>
2018-04-21 14:00:37 -0300

    GH-16 Correção no teste

--- test/george_compiler/tree_test.exs
+++ test/george_compiler/tree_test.exs
@@ -9,19 +9,19 @@ defmodule TreeTest do
     end
 
     test "Criar nó com valor e 1 folha" do
-        assert Tree.new("add") |> Tree.add_leaf(1) == %Tree{value: "add", leafs: [1]}
+        assert Tree.new("add") |> Tree.add_leaf(1) == %Tree{value: "add", leafs: [Tree.new(1)]}
     end
     
     test "Adicionar folha" do
-        assert Tree.new |> Tree.add_leaf(1) == %Tree{leafs: [1]}
+        assert Tree.new |> Tree.add_leaf(1) == %Tree{leafs: [Tree.new(1)]}
     end
 
     test "Adicionar 2 folhas" do
-        assert Tree.new |> Tree.add_leaf(1) |> Tree.add_leaf(2) == %Tree{leafs: [1,2]}
+        assert Tree.new |> Tree.add_leaf(1) |> Tree.add_leaf(2) == %Tree{leafs: [Tree.new(1),Tree.new(2)]}
     end
 
     test "Adicionar 2 folhas e pegar a primeira" do
-        assert Tree.new |> Tree.add_leaf(1) |> Tree.add_leaf(2) |> Tree.get_leaf(1) == 2
+        assert Tree.new |> Tree.add_leaf(1) |> Tree.add_leaf(2) |> Tree.get_leaf(1) == Tree.new(2)
     end
 
     test "Verifica se é folha" do
