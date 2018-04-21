ced0a83e2ccb4ee5a0319b7f32df99a631dcbba5
Alysson Gomes <alyssonggs@hotmail.com>
2018-04-21 19:06:09 -0300

    GH-24 Criação do módulo booleano

---
+++ lib/george_compiler/smc_bool.ex
@@ -0,0 +1,22 @@
+defmodule GeorgeCompiler.SMC.Bool do
+    @operations %{
+        "eq" => &GeorgeCompiler.SMC.Bool.equals/1,
+        "not" => &GeorgeCompiler.SMC.Bool.not_equals/1
+    }
+
+    def equals(s) do
+        {x, y, s} = pop_twice(s)
+        Stack.push(s, x==y)
+    end
+
+    def not_equals(s) do
+        {x, s} = Stack.pop(s)
+        Stack.push(s, not x)
+    end
+
+    def pop_twice(s) do
+        {x, s} = Stack.pop(s)
+        {y, s} = Stack.pop(s)
+        {x, y, s}
+    end
+end
