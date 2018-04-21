114772c58462b96a01bee97dec7d38ab4ab8a3d4
Alysson Gomes <alyssonggs@hotmail.com>
2018-04-21 11:29:34 -0300

    GH-17 Correção nos testes

--- test/george_compiler/smc_test.exs
+++ test/george_compiler/smc_test.exs
@@ -4,12 +4,12 @@ defmodule SMCTest do
   use ExUnit.Case
 
   test "Verifica pilhas" do
-    GeorgeCompiler.SMC.evaluate(Stack.new, Stack.new, Stack.new) == {Stack.new, Stack.new, Stack.new}
+    assert GeorgeCompiler.SMC.evaluate(Stack.new, Stack.new, Stack.new) == {Stack.new, Stack.new, Stack.new}
   end
 
   test "Esvazia pilha c e para" do
     c = Stack.new |> Stack.push(5) |> Stack.push(6)
-    GeorgeCompiler.SMC.evaluate(Stack.new, Stack.new, c) == {Stack.new, Stack.new, Stack.new}
+    assert elem(GeorgeCompiler.SMC.evaluate(Stack.new, Stack.new, c),2) == Stack.new
   end
 
 end
