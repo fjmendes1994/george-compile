e4fe385301fbede23c03df14c6ea84fdffc124ef
Alysson Gomes <alyssonggs@hotmail.com>
2018-04-21 19:13:59 -0300

    GH-24 mais testes

--- test/george_compiler/smc_bool_test.exs
+++ test/george_compiler/smc_bool_test.exs
@@ -14,4 +14,10 @@ defmodule SMCBoolTest do
         s = Stack.new |> Stack.push(true) 
         assert nt(s) == Stack.new |> Stack.push(false)
     end
+
+    #Expressão: !(5==5)
+    test "negação e igualdade" do
+        s = Stack.new |> Stack.push(5) |> Stack.push(5)
+        assert equals(s) |> nt == Stack.new |> Stack.push(false)
+    end
 end
