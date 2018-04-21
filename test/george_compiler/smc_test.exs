926c9c7327170079485c1cbdbd68227d7312403e
Alysson Gomes <alyssonggs@hotmail.com>
2018-04-21 11:20:24 -0300

    GH-17 Criação do teste básico

--- test/george_compiler/smc_test.exs
+++ test/george_compiler/smc_test.exs
@@ -2,4 +2,9 @@ defmodule SMCTest do
   @moduledoc false
 
   use ExUnit.Case
+
+  test "Faz nada" do
+    GeorgeCompiler.SMC.evaluate(Stack.new, Stack.new, Stack.new) == {Stack.new, Stack.new, Stack.new}
+  end
+
 end
