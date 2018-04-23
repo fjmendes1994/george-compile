0f38a65bd405db84e8d41792b7b7c18991138f2d
Alysson Gomes <alyssonggs@hotmail.com>
2018-04-22 22:42:51 -0300

    Refatoração no módulo smc

--- lib/george_compiler/smc/smc.ex
+++ lib/george_compiler/smc/smc.ex
@@ -17,42 +17,41 @@ defmodule GeorgeCompiler.SMC do
         if Tree.is_leaf node do 
             modify_s(node, s, m, c)
         else
-            if is_arit_exp(node.value) do
-                c = arit_decompose_tree(node, c)
-                {s, m, c}
-            else
-                if is_bool_exp(node.value) do
-                    c = bool_decompose_tree(node ,c)
-                    {s, m , c}
-                else 
-                    if is_control(node.value) do
-                        c = control_decompose_tree(node, c)
-                        {s, m , c}
-                    end
-                end
-            end
+            {s, m, decompose_tree(node, c)}
+        end
+    end
+
+    defp decompose_tree(tree, c) do
+        cond do
+            is_arit_exp(tree.value) -> arit_decompose_tree(tree, c)
+            is_bool_exp(tree.value) -> bool_decompose_tree(tree, c)
+            is_control(tree.value) -> control_decompose_tree(tree, c)
         end
     end
 
     defp modify_s(node, s, m, c) do
-        if is_arit_exp(node.value) do
-            s = node.value 
-                |> artit_exp(s)
-            {s, m, c}
+        if is_value node.value do
+            {Stack.push(s,node.value), m, c} 
         else
-            if is_bool_exp(node.value) do
-                s = node.value 
-                    |> bool_exp(s)
-                {s, m, c}
-            else
-                if is_control(node.value) do
-                    {s, m} = node.value 
-                            |> control(s, m)
-                    {s, m, c}
-                else
-                    {Stack.push(s,node.value), m, c} 
-                end
-            end
+            s = get_operation(node.value)
+                |> apply_operation(node.value, s, m, c)
+            {s, m, c}
         end
+    end
+
+    defp is_value(value) do
+        not (is_arit_exp(value) or is_bool_exp(value) or is_control(value)) 
+    end
+
+    defp get_operation(operation) do
+        cond do
+            is_arit_exp(operation) -> &artit_exp/2
+            is_bool_exp(operation) -> &bool_exp/2
+            is_control(operation) -> &control/3
+        end
+    end
+
+    defp apply_operation(function, operation, s, m, c) do
+        function.(operation, s)
     end
 end
