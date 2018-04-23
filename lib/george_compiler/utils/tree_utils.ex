e8944bc07fca000655fba329202513d2366e9cb7
Alysson Gomes <alyssonggs@hotmail.com>
2018-04-22 21:37:18 -0300

    GH-21 Todos os testes para o módulo utilitário de pilha

---
+++ lib/george_compiler/utils/tree_utils.ex
@@ -0,0 +1,7 @@
+defmodule TreeUtils do
+    
+    def remove_first_leaf(tree) do
+        %{tree | leafs: tree.leafs 
+                        |> Enum.drop(1)}
+    end
+end
--- test/george_compiler/utils/stack_utils_test.exs
+++ test/george_compiler/utils/stack_utils_test.exs
@@ -9,4 +9,9 @@ defmodule StackUtilsTest do
                 |> Stack.push(2)
         assert StackUtils.pop_twice(stack) == {2, 5, Stack.new}
     end
+
+    test "Inserir na pilha como árvore" do
+        stack = Stack.new
+        assert StackUtils.push_as_tree(stack, 5) == Stack.new |> Stack.push(Tree.new(5))
+    end
 end
