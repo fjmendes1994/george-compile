cbf344164bd05b8db0e91b7c09058de81ebce0fd
Alysson Gomes <alyssonggs@hotmail.com>
2018-04-29 18:09:03 -0300

    Documentação do módulo aritimético

--- lib/george_compiler/smc/smc_arit.ex
+++ lib/george_compiler/smc/smc_arit.ex
@@ -7,40 +7,54 @@ defmodule GeorgeCompiler.SMC.Arit do
         "rem" => &GeorgeCompiler.SMC.Arit.rem_arit/2
     }
 
+
+    @doc "
+    Função usada pelo módulo SMC para se efetuar a operação (operation). O valor é guardado no topo da pilha.
+    
+    E − E \<m m\' S, M, +/-/* C\> ⇒ \<n S, M, C\>"
     def artit_exp(operation, s, m, c) do
         expression = @operations[operation]
         {expression.(s, m), m, c}
     end
 
+    @doc false
     def add(s, m)do
         {x,y,s} = StackUtils.pop_twice(s)
         Stack.push(s, get_value(x, m) + get_value(y, m))
     end
 
+    @doc false
     def mul(s, m)do
         {x,y,s} = StackUtils.pop_twice(s)
         Stack.push(s, get_value(x, m) * get_value(y, m))
     end
 
-    @doc "Subtração e divisão sendo feitas ao contrário para compensar ordem da pilha"
+    @doc false
+    #Subtração e divisão sendo feitas ao contrário para compensar ordem da pilha
     def sub(s, m)do
         {x,y,s} = StackUtils.pop_twice(s)
         Stack.push(s, get_value(y, m) - get_value(x, m))
     end
 
+    @doc false
     def div(s, m)do
         {x,y,s} = StackUtils.pop_twice(s)
         Stack.push(s, get_value(y, m) / get_value(x, m))
     end
 
+    @doc false
     def rem_arit(s, m)do
         {x,y,s} = StackUtils.pop_twice(s)
         Stack.push(s, rem(get_value(x, m), get_value(y, m)))
     end
 
+    @doc "Confere se a operação está mapeada no módulo"
     def is_arit_exp(operation), do: Map.has_key? @operations, operation
 
-    @doc "Usado para inserir o tipo de operação no topo da pilha antes de empilhar os valores"
+    @doc "
+    Usado para decompor a árvore de operação. Esta função sempre empilha o tipo de operação antes de chamar a função que empilha os valores
+        
+    E − I \<S, M, e +/-/* e\' 0 C\> ⇒ \<S, M, e e\' +/-/* C\>"
     def arit_decompose_tree(tree, s, m, c) do
        {s, m, tree 
                 |> push_values(StackUtils.push_as_tree(c, tree.value))}
--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule GeorgeCompiler.MixProject do
   defp deps do
     [
       {:neotomex, "~> 0.1.7"},
-      {:dbg, "~> 1.0"}
+      {:dbg, "~> 1.0"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
