b92320adfebf707bae6c967b38acb6ede116886d
Alysson <alyssonggs@hotmail.com>
2018-05-26 16:11:28 -0300

    Adicionadas funções para transformação de valor e controle

--- lib/george_compiler/smc/smc.ex
+++ lib/george_compiler/smc/smc.ex
@@ -1,9 +1,31 @@
 defmodule GeorgeCompiler.SMC do
-    defstruct s: Stack.new, m: %{}, c: Stack.new
+	@moduledoc """
+	Estrutura que representa a tripla SMC
+	"""
+	defstruct s: Stack.new, m: %{}, c: Stack.new
 
-    def new() , do: %GeorgeCompiler.SMC{}
+	@doc "Gera uma tripla SMC com duas stacks(valores e controle) e um map(memória)"
+	def new() , do: %GeorgeCompiler.SMC{}
 
-    def add_value(smc, value) do
-        %{smc | s: Stack.push(smc.s, value)}
-    end
+	@doc "Adiciona valor na pilha de valores"
+	def add_value(smc, value) do
+		%{smc | s: Stack.push(smc.s, value)}
+	end
+
+	@doc "Retira um elemento da pilha de valores e retorna o valor e a estrutura SMC sem ele"
+	def pop_value(smc) do
+		{value, new_s} = Stack.pop(smc.s)
+		{value, %{smc | s: new_s}}
+	end
+
+	@doc "Adiciona valor na pilha de controle em forma de árvore"
+	def add_control(smc, value) do
+		%{smc | c: StackUtils.push_as_tree(smc.c, value)}
+	end
+
+	@doc "Retira um elemento da pilha de controle e retorna o elemento e a estrutura SMC sem ele"
+	def pop_control(smc) do
+		{value, new_c} = Stack.pop(smc.c)
+		{value, %{smc | c: new_c}}
+	end
 end
--- test/george_compiler/smc/smc_test.exs
+++ test/george_compiler/smc/smc_test.exs
@@ -1,22 +1,49 @@
 defmodule SMCTest do
-    @moduledoc false
-    alias GeorgeCompiler.SMC, as: SMC
-    use ExUnit.Case
+  @moduledoc false
+  alias GeorgeCompiler.SMC, as: SMC
+  use ExUnit.Case
 
-    test "Criação do SMC" do
-        assert SMC.new == %SMC{
-                            c: %Stack{elements: []},
-                            m: %{},
-                            s: %Stack{elements: []}
-                        }
-    end
+  test "Criação do SMC" do
+    assert SMC.new == %SMC{
+                        c: %Stack{elements: []},
+                        m: %{},
+                        s: %Stack{elements: []}
+                      }
+  end
 
-    test "Inserção em S" do
-        smc = SMC.new |> SMC.add_value(5)
-        assert smc == %SMC{
-                            c: %Stack{elements: []},
-                            m: %{},
-                            s: %Stack{elements: [5]}
-                        }
-    end
+  test "Inserção em S" do
+    smc = SMC.new |> SMC.add_value(5)
+    assert smc == %SMC{
+                      c: %Stack{elements: []},
+                      m: %{},
+                      s: %Stack{elements: [5]}
+                    }
+  end
+
+  test "Remoção de S" do
+    smc = SMC.new |> SMC.add_value(5)
+    assert SMC.pop_value(smc) == {5, %SMC{
+                                      c: %Stack{elements: []},
+                                      m: %{},
+                                      s: %Stack{elements: []}
+                                    }}
+  end
+
+  test "Inserção em C" do
+    smc = SMC.new |> SMC.add_control(5)
+    assert smc == %SMC{
+                      c: %Stack{elements: [Tree.new(5)]},
+                      m: %{},
+                      s: %Stack{elements: []}
+                    }
+  end
+
+  test "Remoção de C" do
+    smc = SMC.new |> SMC.add_control(5)
+    assert SMC.pop_control(smc) == {Tree.new(5), %SMC{
+                                      c: %Stack{elements: []},
+                                      m: %{},
+                                      s: %Stack{elements: []}
+                                    }}
+  end
 end
