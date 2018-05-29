004c7ea5dcd0cef98ce3ec690143d0b15b1026d8
Alysson <alyssonggs@hotmail.com>
2018-05-29 14:30:50 -0300

    Refatoração dos comandos

--- lib/george_compiler/smc/smc_command.ex
+++ lib/george_compiler/smc/smc_command.ex
@@ -8,22 +8,23 @@ defmodule GeorgeCompiler.SMC.Command do
     @doc """
     Função responsável por retornar as funções que efetuam operações na tupla SMC
     """
-    def command(exp, s, m, c) do
+    def command(exp, smc) do
         operation = @operations[exp]
-        operation.(s, m, c)
+        operation.(smc)
     end
 
     @doc """
     C if E < t c c' S, M, if C > ⇒ < S, M, c'' C>
     """
-    def if_command(s, m, c) do
-        {condition, s} = Stack.pop(s)
-        {if_block, s} = Stack.pop(s)
-        {else_block, s} = Stack.pop(s)
+    def if_command(smc) do
+        {condition, if_block, smc} = SMC.pop_twice_value(smc)
+        {else_block, smc} = SMC.pop_value(smc)
         if condition do
-            {s, m, Stack.push(c, if_block)}
+            smc
+            |> SMC.add_control(if_block)
         else
-            {s, m, Stack.push(c, else_block)}
+            smc
+            |> SMC.add_control(else_block)
         end
     end
 
@@ -32,10 +33,9 @@ defmodule GeorgeCompiler.SMC.Command do
     #1 C while E1 < tt b c S, M, while C > ⇒ < S, M, c while b do c C > \n
     #2 C while E2 < ff b c S, M, while C > ⇒ < S, M, C >
     """
-    def while(s, m, c) do
-        {condition, s} = Stack.pop(s)
-        {bool_exp, s} = Stack.pop(s)
-        {code, s} = Stack.pop(s)
+    def while(smc) do
+        {condition, bool_exp, smc} = SMC.pop_twice_value(smc)
+        {code, smc} = SMC.pop_value(smc)
 
         if condition do
             #constroi o while de novo
@@ -44,22 +44,22 @@ defmodule GeorgeCompiler.SMC.Command do
                     |> Tree.add_leaf(bool_exp)
                     |> Tree.add_leaf(code)
             #coloca o comando no topo da pilha c e o while em seguida
-            {s, m, c
-                    |> Stack.push(tree)
-                    |> Stack.push(code)}
+            smc
+            |> SMC.add_control(tree)
+            |> SMC.add_control(code)
         else
             #2
-            {s, m, c}
+            smc
         end
     end
 
     def command_decompose_tree(tree, s, m, c) do
-        c = c
-            |> StackUtils.push_as_tree(tree.value)
+        smc = smc
+              |>  SMC.add_control(tree.value)
         case tree.value do
-            :if -> if_decompose(tree, s, m, c)
-            :while -> while_decompose(tree, s, m, c)
-            :seq -> sequence_decompose(tree, s, m, c)
+            :if -> if_decompose(tree, smc)
+            :while -> while_decompose(tree, smc)
+            :seq -> sequence_decompose(tree, smc)
         end
     end
 
@@ -67,13 +67,11 @@ defmodule GeorgeCompiler.SMC.Command do
     Decomposição da aŕvore de if. \n
     C if I < S, M, if b then c else c' C > ⇒ < c c' S, M, b if C >
     """
-    def if_decompose(tree, s, m, c) do
-        s = s
-            |> Stack.push(Enum.at(tree.leafs, 2))
-            |> Stack.push(Enum.at(tree.leafs, 1))
-        c = c
-            |> Stack.push(Enum.at(tree.leafs, 0))
-        {s, m, c}
+    def if_decompose(tree, smc) do
+        smc
+        |> SMC.add_value(Enum.at(tree.leafs, 2))
+        |> SMC.add_value(Enum.at(tree.leafs, 1))
+        |> SMC.add_control(Enum.at(tree.leafs, 0))
     end
 
      
@@ -81,23 +79,22 @@ defmodule GeorgeCompiler.SMC.Command do
     Decomposição da aŕvore de while. \n
     #1 C while I < S, M, while b do c C > ⇒ < b c S, M, b while C > \n
     """
-    def while_decompose(tree, s, m, c) do
-        s = s
-            |> Stack.push(Enum.at(tree.leafs, 1))
-            |> Stack.push(Enum.at(tree.leafs, 0))
-        c = c
-            |> Stack.push(Enum.at(tree.leafs, 0))
-        {s, m, c}
+    def while_decompose(tree, smc) do
+        smc
+        |> SMC.add_value(Enum.at(tree.leafs, 1))
+        |> SMC.add_value(Enum.at(tree.leafs, 0))
+        |> SMC.add_control(Enum.at(tree.leafs, 0))
     end
 
     @doc """
     C; < S, M, c; c' C > ⇒ < S, M, c c' C >
     """
-    def sequence_decompose(tree, s, m, c) do
-        {_, c} = Stack.pop(c)
-        {s, m, c
-                |> Stack.push(Enum.at(tree.leafs, 1))
-                |> Stack.push(Enum.at(tree.leafs, 0))}
+    def sequence_decompose(tree, smc) do
+        {_, smc} = SMC.pop_control(smc)
+        
+        smc
+        |> SMC.add_control(Enum.at(tree.leafs, 1))
+        |> SMC.add_control(Enum.at(tree.leafs, 0))
     end
 
     @doc "Verifica se a operação está mapeada no módulo"
