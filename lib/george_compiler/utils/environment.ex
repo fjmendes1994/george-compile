092e31114ff1ca8796a7b0ceb130bc6dd8a08b83
Alysson <alyssonggs@hotmail.com>
2018-06-10 14:55:04 -0300

    GH-42 Adicionada função para constante no ambiente

--- lib/george_compiler/utils/environment.ex
+++ lib/george_compiler/utils/environment.ex
@@ -9,6 +9,10 @@ defmodule Environment do
         %{environment | refs: [{String.to_atom(ref), SecureRandom.uuid} | environment.refs]}
     end
 
+    def add_const(environment, id, value) do
+        %{environment | refs: [{String.to_atom(id), value} | environment.refs]}
+    end
+
     def get_address(environment, ref) do
         Keyword.get(environment.refs, String.to_atom(ref))
     end
--- test/george_compiler/utils/environment_test.exs
+++ test/george_compiler/utils/environment_test.exs
@@ -15,21 +15,25 @@ defmodule EnvironmentTest do
 	end
 
 	test "Adição de referência" do
-  	assert %Environment{refs: [ab: _]} = Environment.new() |> Environment.add_ref("ab")
+  		assert %Environment{refs: [ab: _]} = Environment.new() |> Environment.add_ref("ab")
 	end
 
+	test "Adição de constante" do
+		assert %Environment{refs: [ab: 5]} == Environment.new() |> Environment.add_const("ab",5)
+  	end
+
 	test "Recupera endereço" do
-  ambient = Environment.new() |> Environment.add_ref("ab")
-  	assert Environment.get_address(ambient, "ab") == ambient.refs[String.to_atom("ab")]
+  		ambient = Environment.new() |> Environment.add_ref("ab")
+  		assert Environment.get_address(ambient, "ab") == ambient.refs[String.to_atom("ab")]
 	end
 
 	test "Recupera contagem" do
-  	ambient = Environment.new() |> Environment.add_ref("ab") |> Environment.add_ref("bc")
-  	assert Environment.get_length(ambient) == 2
+  		ambient = Environment.new() |> Environment.add_ref("ab") |> Environment.add_ref("bc")
+  		assert Environment.get_length(ambient) == 2
 	end
 
 	test "Recupera mais recente" do
-  	ambient = Environment.new() |> Environment.add_ref("ab") |> Environment.add_ref("ab")
-  	assert Environment.get_address(ambient, "ab") ==  ambient.refs[:ab]
+  		ambient = Environment.new() |> Environment.add_ref("ab") |> Environment.add_ref("ab")
+  		assert Environment.get_address(ambient, "ab") ==  ambient.refs[:ab]
 	end
 end
