a374e4be84c8c19c2699de2451dfb2d77ef80ded
Fernando José da Silva Mendes <fernandomendes@midiacom.uff.br>
2018-06-12 20:16:41 -0300

    Corrigindo erro de digitaçao

--- test/george_compiler/smc/smc_decl_test.exs
+++ test/george_compiler/smc/smc_decl_test.exs
@@ -2,25 +2,25 @@ defmodule SMCDeclTest do
     @moduledoc false
     alias GeorgeCompiler.SMC.Decl, as: Decl
     alias GeorgeCompiler.SMC, as: SMC
-    
+
     use ExUnit.Case
-    
+
     test "Decomposiçáo da árvore" do
         smc = SMC.new
               |> SMC.add_control(Tree.new(:ref)
                                  |> Tree.add_leaf("init")
-                                 |> Tree.add_leaf("2"))      
+                                 |> Tree.add_leaf("2"))
               |> SMC.add_control(Tree.new(:ref)
                                 |> Tree.add_leaf("var")
                                 |> Tree.add_leaf("5"))
-              
+
         tree = Tree.new(:decl)
                         |> Tree.add_leaf(Tree.new(:ref)
                                         |> Tree.add_leaf("var")
                                         |> Tree.add_leaf("5"))
                         |> Tree.add_leaf(Tree.new(:ref)
                                         |> Tree.add_leaf("init")
-                                        |> Tree.add_leaf("2"))        
+                                        |> Tree.add_leaf("2"))
         assert Decl.decl_decompose_tree(tree, SMC.new) == smc
     end
 
@@ -29,10 +29,10 @@ defmodule SMCDeclTest do
               |> SMC.add_control(:ref)
               |> SMC.add_control(5)
               |> SMC.add_value("var")
-              
+
         tree = Tree.new(:ref)
                 |> Tree.add_leaf("var")
-                |> Tree.add_leaf(5)   
+                |> Tree.add_leaf(5)
         assert Decl.decl_decompose_tree(tree, SMC.new) == smc
     end
 
@@ -52,7 +52,7 @@ defmodule SMCDeclTest do
               |> SMC.add_value("var")
               |> SMC.add_value(5)
 
-        ref = Decl.const(smc)
+        ref = Decl.cns(smc)
 
         assert %SMC{e: %Environment{refs: [var: 5]}} == ref
     end
